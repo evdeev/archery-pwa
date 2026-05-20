4602812721b8d69199841aac101a697ce5417559
Boris Evdeev <evdeev@gmail.com>
2026-05-20 09:43:29 +0300

    fix: wrap full app in router

--- src/react/App.jsx
+++ src/react/App.jsx
@@ -1,18 +1,22 @@
+import { BrowserRouter } from 'react-router-dom';
+
 import { AppRouter } from './router/AppRouter.jsx';
 import { TabBar } from './components/navigation/TabBar.jsx';
 
 export function App() {
   return (
-    <div className="app-shell">
-      <header className="top-bar">
-        <h1>Archery Scores</h1>
-      </header>
+    <BrowserRouter basename="/archery-pwa/">
+      <div className="app-shell">
+        <header className="top-bar">
+          <h1>Archery Scores</h1>
+        </header>
 
-      <main className="screen-container">
-        <AppRouter />
-      </main>
+        <main className="screen-container">
+          <AppRouter />
+        </main>
 
-      <TabBar />
-    </div>
+        <TabBar />
+      </div>
+    </BrowserRouter>
   );
 }
