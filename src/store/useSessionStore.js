b123df541579adad29e11fef978a00c312d97f6e
Boris Evdeev <evdeev@gmail.com>
2026-05-20 09:09:36 +0300

    feat: add auto focus and session settings state

--- src/store/useSessionStore.js
+++ src/store/useSessionStore.js
@@ -2,9 +2,20 @@ import { create } from 'zustand';
 
 import { createEmptySession } from '../domain/session-engine/session-factory.js';
 import { calculateSessionScore } from '../domain/session-engine/score-utils.js';
+import { getNextShotPosition } from '../domain/session-engine/sequential-input.js';
 
 export const useSessionStore = create((set, get) => ({
   currentSession: createEmptySession(),
+
+  selectedShot: {
+    roundIndex: 0,
+    seriesIndex: 0,
+    shotIndex: 0
+  },
+
+  setSelectedShot(position) {
+    set({ selectedShot: position });
+  },
 
   setShot(roundIndex, seriesIndex, shotIndex, value) {
     const session = structuredClone(get().currentSession);
@@ -18,6 +29,33 @@ export const useSessionStore = create((set, get) => ({
     session.totalScore = result.total;
     session.xCount = result.xCount;
     session.tenCount = result.tenCount;
+
+    const nextShot = getNextShotPosition(session, {
+      roundIndex,
+      seriesIndex,
+      shotIndex
+    });
+
+    set({
+      currentSession: session,
+      selectedShot: nextShot
+    });
+  },
+
+  setSeriesComment(roundIndex, seriesIndex, comment) {
+    const session = structuredClone(get().currentSession);
+
+    session.rounds[roundIndex]
+      .series[seriesIndex]
+      .comment = comment;
+
+    set({ currentSession: session });
+  },
+
+  updateSessionSettings(settings) {
+    const session = structuredClone(get().currentSession);
+
+    Object.assign(session, settings);
 
     set({ currentSession: session });
   }
