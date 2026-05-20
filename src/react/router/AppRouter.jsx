a01e471f6fa4017ed18c8344c50602c6f2742b5c
Boris Evdeev <evdeev@gmail.com>
2026-05-20 09:13:03 +0300

    feat: add real screen routing

--- src/react/router/AppRouter.jsx
+++ src/react/router/AppRouter.jsx
@@ -1,12 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { HistoryScreen } from '../screens/HistoryScreen.jsx';
+import { StatisticsScreen } from '../screens/StatisticsScreen.jsx';
+import { EquipmentScreen } from '../screens/EquipmentScreen.jsx';
+import { SessionEditorScreen } from '../screens/SessionEditorScreen.jsx';
+import { ImportExportScreen } from '../screens/ImportExportScreen.jsx';
 
 export function AppRouter() {
   return (
     <BrowserRouter basename="/archery-pwa/">
       <Routes>
         <Route path="/" element={<HistoryScreen />} />
+        <Route path="/session" element={<SessionEditorScreen />} />
+        <Route path="/statistics" element={<StatisticsScreen />} />
+        <Route path="/equipment" element={<EquipmentScreen />} />
+        <Route path="/settings" element={<ImportExportScreen />} />
       </Routes>
     </BrowserRouter>
   );
