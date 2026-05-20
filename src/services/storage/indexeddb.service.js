055b5e9e0f19814d273a9392c64804270d979b5d
Boris Evdeev <evdeev@gmail.com>
2026-05-20 09:20:03 +0300

    feat: extend IndexedDB stores

--- src/services/storage/indexeddb.service.js
+++ src/services/storage/indexeddb.service.js
@@ -1,13 +1,25 @@
 import { openDB } from 'idb';
 
 const DATABASE_NAME = 'archery-pwa';
-const DATABASE_VERSION = 1;
+const DATABASE_VERSION = 2;
 
 export async function getDatabase() {
   return openDB(DATABASE_NAME, DATABASE_VERSION, {
     upgrade(database) {
       if (!database.objectStoreNames.contains('sessions')) {
         database.createObjectStore('sessions', {
+          keyPath: 'id'
+        });
+      }
+
+      if (!database.objectStoreNames.contains('settings')) {
+        database.createObjectStore('settings', {
+          keyPath: 'id'
+        });
+      }
+
+      if (!database.objectStoreNames.contains('equipment')) {
+        database.createObjectStore('equipment', {
           keyPath: 'id'
         });
       }
