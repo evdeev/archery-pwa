a88e3b19ea2363ab522c563656903a38f1ee6ef3
Boris Evdeev <evdeev@gmail.com>
2026-05-20 09:00:01 +0300

    feat: add import export UI

---
+++ src/react/screens/ImportExportScreen.jsx
@@ -0,0 +1,10 @@
+export function ImportExportScreen() {
+  return (
+    <section className="import-export-screen">
+      <button type="button">Импортировать историю</button>
+      <button type="button">Экспортировать историю</button>
+      <button type="button">Импортировать экипировку</button>
+      <button type="button">Экспортировать экипировку</button>
+    </section>
+  );
+}
