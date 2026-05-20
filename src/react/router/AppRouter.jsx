4ca3236137124dd01fc1c18b5ba25b5feab20c37
Boris Evdeev <evdeev@gmail.com>
2026-05-20 09:38:15 +0300

    perf: lazy load route screens

--- src/react/router/AppRouter.jsx
+++ src/react/router/AppRouter.jsx
@@ -1,21 +1,28 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { HistoryScreen } from '../screens/HistoryScreen.jsx';
-import { StatisticsScreen } from '../screens/StatisticsScreen.jsx';
-import { EquipmentScreen } from '../screens/EquipmentScreen.jsx';
-import { SessionEditorScreen } from '../screens/SessionEditorScreen.jsx';
-import { ImportExportScreen } from '../screens/ImportExportScreen.jsx';
+import { LoadingState } from '../components/feedback/LoadingState.jsx';
+
+const HistoryScreen = lazy(() => import('../screens/HistoryScreen.jsx').then(module => ({ default: module.HistoryScreen })));
+const StatisticsScreen = lazy(() => import('../screens/StatisticsScreen.jsx').then(module => ({ default: module.StatisticsScreen })));
+const EquipmentScreen = lazy(() => import('../screens/EquipmentScreen.jsx').then(module => ({ default: module.EquipmentScreen })));
+const SessionEditorScreen = lazy(() => import('../screens/SessionEditorScreen.jsx').then(module => ({ default: module.SessionEditorScreen })));
+const SettingsScreen = lazy(() => import('../screens/SettingsScreen.jsx').then(module => ({ default: module.SettingsScreen })));
+const OnboardingScreen = lazy(() => import('../screens/OnboardingScreen.jsx').then(module => ({ default: module.OnboardingScreen })));
 
 export function AppRouter() {
   return (
     <BrowserRouter basename="/archery-pwa/">
-      <Routes>
-        <Route path="/" element={<HistoryScreen />} />
-        <Route path="/session" element={<SessionEditorScreen />} />
-        <Route path="/statistics" element={<StatisticsScreen />} />
-        <Route path="/equipment" element={<EquipmentScreen />} />
-        <Route path="/settings" element={<ImportExportScreen />} />
-      </Routes>
+      <Suspense fallback={<LoadingState />}>
+        <Routes>
+          <Route path="/" element={<HistoryScreen />} />
+          <Route path="/onboarding" element={<OnboardingScreen />} />
+          <Route path="/session" element={<SessionEditorScreen />} />
+          <Route path="/statistics" element={<StatisticsScreen />} />
+          <Route path="/equipment" element={<EquipmentScreen />} />
+          <Route path="/settings" element={<SettingsScreen />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
